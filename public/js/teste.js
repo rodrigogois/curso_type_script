da15ecbc9fa05455386df399f913a325c0c90aee
Rodrigo Argolo <rodrigo.argolo@outlook.com>
2019-01-15 22:54:17 -0300

    aula 5 - como não organizar requisições AJAX

--- public/js/teste.js
+++ public/js/teste.js
@@ -1,4 +1,6 @@
-define(["require", "exports", "module1"], function (require, exports) {
+define(["require", "exports", "http/post-http", "module1"], function (require, exports, post_http_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
+    new post_http_1.default().query();
 });
+//# sourceMappingURL=teste.js.map
